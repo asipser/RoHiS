e39c53197dc415fb7e27c8392f530a8bb4884805
Rupayan <rneogy@mit.edu>
2016-01-19 15:31:34 -0500

    venmo fixes, settings fixes, top bar partial

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,40 +1,36 @@
-$('a.item').click(function(){
-	$('a.item').removeClass("active");
-	$(this).addClass("active");
-});
-$('#getstartedbutton').click(function(){
-	$('.ui.modal.signup').modal('show');
-});
-$('#loginbutton').click(function(){
-	$('.ui.modal.login').modal('show');
-});
-$('#aboutmenuitem').click(function(){
-	$('#aboutpanel').show();
-	$('html,body').animate({
-		scrollTop: window.innerHeight
-	}, 1000);
-});
-$('#downarrow').click(function(){
-	$('#aboutpanel').show();
-	$('html,body').animate({
-		scrollTop: window.innerHeight
-	}, 1000);
-});
-var $win = $(window);
-$win.scroll(function(){
-	if($win.scrollTop() == 0){
-		$('#aboutpanel').hide();
+$(document).ready(function(){
+	$('a.item').click(function(){
 		$('a.item').removeClass("active");
-		$('#homemenuitem').addClass("active");
-	}
-});
-$('#sidebarmenuitem').click(function(){
-	$('div.ui.sidebar').sidebar('toggle');
-});
-$('.ui.accordion.title').click(function(){
-	console.log('fslkjfl');
-	if($(this).hasClass('active')){
-		$('div.accordion.title').removeClass('active');
-		$(this).addClass('active');
-	}
+		$(this).addClass("active");
+	});
+	$('#getstartedbutton').click(function(){
+		$('.ui.modal.signup').modal('show');
+	});
+	$('#loginbutton').click(function(){
+		$('.ui.modal.login').modal('show');
+	});
+	$('#aboutmenuitem').click(function(){
+		$('#aboutpanel').show();
+		$('html,body').animate({
+			scrollTop: window.innerHeight
+		}, 1000);
+	});
+	$('#downarrow').click(function(){
+		$('#aboutpanel').show();
+		$('html,body').animate({
+			scrollTop: window.innerHeight
+		}, 1000);
+	});
+	var $win = $(window);
+	$win.scroll(function(){
+		if($win.scrollTop() == 0){
+			$('#aboutpanel').hide();
+			$('a.item').removeClass("active");
+			$('#homemenuitem').addClass("active");
+		}
+	});
+	$('#sidebarmenuitem').click(function(){
+		$('div.ui.sidebar').sidebar('toggle');
+	});
+	$('.ui.accordion').accordion();
 });
